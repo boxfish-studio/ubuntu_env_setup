19e84ca33a6019c93669c257e5ab088c4b32f54d
Agustin Tena <atena@boxfish.studio>
2019-03-30 16:12:04 +0100

    added g++ and unrar to start.sh

--- 16.04/start.sh
+++ 16.04/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install -y build-essential git git-core gitg unzip zsh curl xclip
+sudo apt-get install -y build-essential git git-core gitg unzip unrar zsh curl xclip g++
 
 #install xpm (xpacks package manager)
 sudo npm install --global xpm
